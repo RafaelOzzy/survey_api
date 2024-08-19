0a5164066f6872d071b3278877b6fbb8f647b3d4
RafaelOzzy <rafael.ramos.s@gmail.com>
2024-08-19 11:21:51 -0300

    final deploy

--- app/graphql/mutations/reorder_questions.rb
+++ app/graphql/mutations/reorder_questions.rb
@@ -1,24 +1,3 @@
-# module Mutations
-#   class ReorderQuestions < Mutations::BaseMutation
-#     argument :survey_id, ID, required: true
-#     argument :question_order, [ID], required: true
-
-#     field :survey, Types::SurveyType, null: false
-
-#     def resolve(survey_id:, question_order:)
-#       survey = Survey.find(survey_id)
-#       ActiveRecord::Base.transaction do
-#         question_order.each_with_index do |id, index|
-#           Question.find(id).update!(position: index + 1)
-#         end
-#       end
-
-#       { survey: survey.reload }
-#     rescue ActiveRecord::RecordInvalid => e
-#       GraphQL::ExecutionError.new(e.record.errors.full_messages.join(", "))
-#     end
-#   end
-# end
 module Mutations
   class ReorderQuestions < Mutations::BaseMutation
     argument :survey_id, ID, required: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User < ApplicationRecord
-  # Inclui os módulos do Devise que você precisa
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Define os papéis de usuário
   ROLES = {
     coordinator: 'coordinator',
     respondent: 'respondent'
   }.freeze
 
-  # Validações
   validates :role, inclusion: { in: ROLES.values }
 
-  # Métodos para verificar os papéis
   def coordinator?
     role == ROLES[:coordinator]
   end
